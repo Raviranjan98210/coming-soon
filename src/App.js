e1e3d25c09c9dfc6a70157f8a57767623b910e77
Ravi Ranjan <raviranjan98210@gmail.com>
2021-03-28 07:33:30 +0530

    coming soon page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
-import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
-import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+import React from "react";
+import { ReactComponent as IconFacebook } from "./assets/icons/facebook.svg";
+import { ReactComponent as IconTwitter } from "./assets/icons/twitter.svg";
+import { ReactComponent as IconGithub } from "./assets/icons/github.svg";
 import "./App.css";
 
 class App extends React.Component {
@@ -10,35 +10,29 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LOGO</a>
+            <a href=".">Youtimo</a>
           </div>
           <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
-              <IconFacebook className="icon" />
-            </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
-              <IconTwitter className="icon" />
-            </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
-              <IconGithub className="icon" />
-            </a>
+            <h1 style={{ textAlign: "center", fontWeight: "800", fontSize: "20px" }}>
+              6003543391
+            </h1>
           </div>
         </div>
         <div className="content">
           <div className="title-holder">
-            <h1>Get ready for the change.</h1>
-            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+            <h1>We are coming soon.</h1>
+            <p>
+              Website coming soon. Please check back to know more. Shoot us an email if
+              you're curious.
+            </p>
           </div>
-          <a href="mailto:user@example.com">
+          <a href="mailto:youtimoindia@gmail.com">
             <div className="cta">Send us an email</div>
           </a>
         </div>
-        <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
-        </div>
       </div>
     );
-  }
+  };
 }
 
 export default App;
