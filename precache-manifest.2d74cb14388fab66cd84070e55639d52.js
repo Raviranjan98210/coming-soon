eafd6ece0bfa52fbb786913dd8c2fdd1a4b1ecfb
Raviranjan98210 <Raviranjan98210@users.noreply.github.com>
2021-03-28 02:44:14 +0000

    last commit e2bf20117d95a8992907569b580d920bb56876a7

---
+++ precache-manifest.2d74cb14388fab66cd84070e55639d52.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a2034fccc5a866214b79f26d0fce31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19cd78aaae484e77bff3",
+    "url": "/static/css/main.ea896748.chunk.css"
+  },
+  {
+    "revision": "ed93eeddf9369950c440",
+    "url": "/static/js/2.58aaed71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58aaed71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19cd78aaae484e77bff3",
+    "url": "/static/js/main.f8c6581f.chunk.js"
+  },
+  {
+    "revision": "8cb4ec54fa2007826be9",
+    "url": "/static/js/runtime-main.05172a96.js"
+  },
+  {
+    "revision": "5159a5d89abe8bf68b09b569dbeccbc0",
+    "url": "/static/media/IBMPlexSans-Bold.5159a5d8.ttf"
+  },
+  {
+    "revision": "ee425cc83f37323665790c89758cf359",
+    "url": "/static/media/IBMPlexSans-BoldItalic.ee425cc8.ttf"
+  },
+  {
+    "revision": "40bbef08ca6f6edea2a9a9e882541ce0",
+    "url": "/static/media/IBMPlexSans-Italic.40bbef08.ttf"
+  },
+  {
+    "revision": "ee83103a4a777209b0f759a4ff598066",
+    "url": "/static/media/IBMPlexSans-Medium.ee83103a.ttf"
+  },
+  {
+    "revision": "eb7dadea8e7c37ce1a1406045dda7c1e",
+    "url": "/static/media/IBMPlexSans-MediumItalic.eb7dadea.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "/static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  }
+]);
